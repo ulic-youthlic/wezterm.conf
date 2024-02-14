e09e97b95a6167e2af4bde36c79f0d3c306018f2
ulic-youthlic <Ulic.youthlic@gmail.com>
2024-02-14 16:37:19 +0800

    move options from wezterm.lua into options.lua

---
+++ options.lua
@@ -0,0 +1,82 @@
+local wezterm = require("wezterm")
+local is_windows = require("fn").is_windows
+
+local fonts = {
+    {
+        family = [[FiraCode Nerd Font]],
+        weight = [[Thin]],
+        assume_emoji_presentation = false,
+        harfbuzz_features = {
+            "calt=1",
+            "clig=1",
+            "liga=1",
+            "cv01",
+            "cv02",
+            "cv06",
+            "zero",
+            "onum",
+            "cv17",
+            "ss05",
+            "ss03",
+            "cv16",
+            "cv31",
+            "cv29",
+            "cv30",
+        },
+    },
+}
+local font_size
+local animation_fps
+local default_prog
+
+if is_windows() then
+    fonts[2] = {
+        family = [[Twitter Color Emoji]],
+        assume_emoji_presentation = true,
+    }
+    font_size = 16.0
+    animation_fps = 60
+    default_prog = {
+        [[pwsh]]
+    }
+else
+    fonts[2] = {
+        family = [[Noto Sans CJK SC]],
+        assume_emoji_presentation = false,
+    }
+    fonts[3] = {
+        family = [[Noto Color Emiji]],
+        assume_emoji_presentation = true,
+    }
+    font_size = 14.0
+    animation_fps = 1
+end
+
+
+local M = {
+    automatically_reload_config = true,
+    cell_width = 1.0,
+    line_height = 1.05,
+    audible_bell = "Disabled",
+    tab_bar_at_bottom = true,
+    use_fancy_tab_bar = false,
+    hide_tab_bar_if_only_one_tab = true,
+    window_decorations = "RESIZE|TITLE",
+    underline_thickness = 3.0,
+    enable_scroll_bar = true,
+    color_scheme = "MaterialOcean",
+    window_background_opacity = 0.9,
+    window_background_image_hsb = {
+        brightness = 0.02,
+        hue = 1.0,
+        saturation = 1.0,
+    },
+    text_background_opacity = 0.6,
+    font = wezterm.font_with_fallback(fonts),
+    font_size = font_size,
+    animation_fps = animation_fps,
+    default_prog = default_prog,
+}
+
+
+return M
--- wezterm.lua
+++ wezterm.lua
@@ -1,24 +1,3 @@
-local wezterm = require("wezterm")
-
-local config = wezterm.config_builder()
-
-config.automatically_reload_config = true
-config.cell_width = 1.0
-config.line_height = 1.05
-config.audible_bell = "Disabled"
-config.tab_bar_at_bottom = true
-config.use_fancy_tab_bar = false
-config.hide_tab_bar_if_only_one_tab = true
-config.window_decorations = "RESIZE|TITLE"
-config.underline_thickness = 3.0
-config.enable_scroll_bar = true
-config.color_scheme = "MaterialOcean"
-config.window_background_image_hsb = {
-    brightness = 0.02,
-    hue = 1.0,
-    saturation = 1.0,
-}
-config.text_background_opacity = 0.6
-
-
-return config
+return require("config"):init()
+    :append(require("options"))
+    .options
