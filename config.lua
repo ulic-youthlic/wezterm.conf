9e15be192c678f642f33115ffc207b49307441ed
ulic-youthlic <Ulic.youthlic@gmail.com>
2024-02-14 16:36:33 +0800

    add config.lua for class Config and its methods, init and append

---
+++ config.lua
@@ -0,0 +1,37 @@
+local wezterm = require("wezterm")
+
+---@class Config
+---@filed options table
+local Config = {}
+
+---Init Config
+---@return Config
+function Config:init()
+    local o = {}
+    self = setmetatable(o, { __index = Config })
+    self.options = {}
+    return o
+end
+
+---Append to `Config.options`
+---@param new_options table new options to append
+---@return Config
+function Config:append(new_options)
+    for key, val in pairs(new_options) do
+        if self.options[key] ~= nil then
+            wezterm.log_warn(
+                "Duplicate config option detected: ",
+                {
+                    old = self.options[key],
+                    new = new_options[key]
+                }
+            )
+            goto continue
+        end
+        self.options[key] = val
+        ::continue::
+    end
+    return self
+end
+
+return Config
