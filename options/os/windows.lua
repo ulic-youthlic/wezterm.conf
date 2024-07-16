33641cec4e8cc018526a6a5c830e94369aeada92
ulic-youthlic <ulic.youthlic@gmail.com>
2024-07-16 15:18:35 +0800

    Add font rules for Windows OS.

--- options/os/windows.lua
+++ options/os/windows.lua
@@ -13,4 +13,19 @@ return {
     [[pwsh]],
   },
   font = wezterm.font_with_fallback(fonts),
+  font_rules = {
+    {
+      italic = true,
+      font = wezterm.font_with_fallback({
+        require("fonts.victor_mono_nerd_font"),
+      }),
+    },
+    {
+      italic = false,
+      font = wezterm.font_with_fallback({
+        require("fonts.firacode_nerd_font"),
+        require("fonts.noto_sans_cjk_sc"),
+      }),
+    },
+  },
 }
